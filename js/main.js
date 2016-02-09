dfababb96781495caba2708d6ab5a402d5a6449f
jlawson5 <jlawson5@masonlive.gmu.edu>
2016-02-09 00:37:11 -0500

    Prototype in working condition!
    Through blood, sweat, and tears, the Prototype works.

--- js/main.js
+++ js/main.js
@@ -6,27 +6,33 @@
 
     window.onload = function() {
 
-        var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+        var game = new Phaser.Game(600, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles-1', 'assets/tiles-1.png');
+    game.load.image('platform', 'assets/GravityManPlatform.png');//Floor and Platform objects are not working properly//
+    game.load.image('floor', 'assets/GravityManFloor.png');
     game.load.spritesheet('gravityMan', 'assets/GravityMan.png', 32, 32);//Gravity Man: The player character//
     game.load.image('coin', 'assets/GravityManCoin.png');//Coins the player must find and collect//
     game.load.image('background', 'assets/GravityManBG.png');//Background image//
-
+    game.load.audio('coinCollect', 'assets/Electronic_Chime-KevanGC-495939803.mp3');
+    game.load.audio('gravityChange', 'assets/spin_jump-Brandino480-2020916281.mp3');
 }
 
-var map;
-var tileset;
-var layer;
 var player;
 var facing = 'left';
 var jumpTimer = 0;
 var cursors;
 var jumpButton;
 var bg;
+var g_dir = 'down';//current direction of gravity//
+var coinSound;
+var gSound;
+var coins;
+var score = 0;
+var scoreString = '';
+var scoreText;
+var floor;//Not working//
 
 function create() {
 
@@ -37,32 +43,33 @@ function create() {
     bg = game.add.tileSprite(0, 0, 800, 600, 'background');
     bg.fixedToCamera = true;
 
-    map = game.add.tilemap('level1');
-
-    map.addTilesetImage('tiles-1');
-
-    map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
-
-    layer = map.createLayer('Tile Layer 1');
-
-    //  Un-comment this on to see the collision tiles
-    // layer.debug = true;
-
-    layer.resizeWorld();
-
     game.physics.arcade.gravity.y = 250;
+    
+    //adds sounds to the game//
+    coinSound = game.add.audio('coinCollect');
+    gSound = game.add.audio('gravityChange');
+    coinSound.allowMultiple = true;
+    gSound.allowMultiple = true;
+    coinSound.addMarker('coinGet', 0, 2.0);
+    gSound.addMarker('gSwitch', 0, 0.5);
 
     player = game.add.sprite(32, 32, 'gravityMan');
-    game.add.sprite(0, 0, 'background')
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     player.body.bounce.y = 0.2;
     player.body.collideWorldBounds = true;
-    player.body.setSize(20, 32, 5, 16);
+    player.body.setSize(16, 32, 8, 0);
 
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    player.animations.add('left', [2, 3], 10, true);
+    player.animations.add('right', [0, 1], 10, true);
+    player.animations.add('left_up', [6, 7], 10, true);//player is facing left and gravity is up//
+    player.animations.add('right_up', [4, 5], 10, true);//player is facing right and gravity is up//
+    
+    coins = game.add.group();
+    createCoins();
+    
+    scoreString = 'Score: ';
+    scoreText = game.add.text(10, 10, scoreString + score, {font: '34px Arial', fill: '#fff'});
 
     game.camera.follow(player);
 
@@ -73,7 +80,7 @@ function create() {
 
 function update() {
 
-    game.physics.arcade.collide(player, layer);
+    game.physics.arcade.collide(player, floor);
 
     player.body.velocity.x = 0;
 
@@ -81,47 +88,105 @@ function update() {
     {
         player.body.velocity.x = -150;
 
-        if (facing != 'left')
-        {
-            player.animations.play('left');
+        //if (facing != 'left')
+        //{
+            if(g_dir == 'down')
+                player.animations.play('left');
+            else
+                player.animations.play('left_up');
             facing = 'left';
-        }
+        //}
     }
     else if (cursors.right.isDown)
     {
         player.body.velocity.x = 150;
 
-        if (facing != 'right')
-        {
-            player.animations.play('right');
+        //if (facing != 'right')
+        //{
+            if(g_dir == 'down')
+                player.animations.play('right');
+            else
+                player.animations.play('right_up');
             facing = 'right';
-        }
+        //}
     }
     else
     {
-        if (facing != 'idle')
-        {
+        //if (facing != 'idle')
+        //{
             player.animations.stop();
 
             if (facing == 'left')
             {
-                player.frame = 0;
+                if(g_dir == 'down')
+                    player.frame = 2;
+                else
+                    player.frame = 6;
             }
-            else
+            else if(facing == 'right')
             {
-                player.frame = 5;
+                if(g_dir == 'down')
+                    player.frame = 0;
+                else
+                    player.frame = 4;
             }
 
             facing = 'idle';
-        }
+        //}
     }
     
-    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+    if (jumpButton.isDown && game.time.now > jumpTimer)
     {
-        player.body.velocity.y = -250;
+        //player.body.velocity.y = -250;
+        game.physics.arcade.gravity.y *= -1;
+        if (g_dir == 'down')//if gravity is acting downwards, set gravity to 'up'//
+        {
+            g_dir = 'up';
+        }
+        else
+        {
+            g_dir = 'down';
+        }
+        gSound.play('gSwitch');
         jumpTimer = game.time.now + 750;
     }
+    
+    //Check for and handles coin collisions//
+    game.physics.arcade.overlap(player, coins, coinCollision, null, this);
+}
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//function createCoins(): spawns 15 coins to random coordinates with random bounce (coins will share the players gravity).  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function createCoins()
+{
+    for(var i = 0; i < 15; i++)
+    {
+        var coin = coins.create((Math.random() * 600) + 1, 16, 'coin');
+        game.physics.enable(coin, Phaser.Physics.ARCADE);
+        coin.body.bounce.y = 1;
+        coin.body.setSize(16, 16, 0, 0);
+        coin.body.collideWorldBounds = true;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//function coinCollision(coin, player)                                                                 //
+//destroys the coin and adds 100 to the score. Spawns another coin with random bounce and x-coordinates//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function coinCollision(player, coin)
+{
+    coin.kill();
+    
+    coinSound.play('coinGet');
+    score += 100;
+    scoreText.text = scoreString + score;
+    
+    var newCoin = coins.create((Math.random() * 600) + 1, 16, 'coin');
+    game.physics.enable(newCoin, Phaser.Physics.ARCADE);
+    newCoin.body.bounce.y = 1;
+    newCoin.body.setSize(16, 16, 0, 0);
+    newCoin.body.collideWorldBounds = true;
 }
 
 function render () {
